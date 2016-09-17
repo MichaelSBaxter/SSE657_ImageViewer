d7ca16a37b5a59a11f1da457c88851a1b01e01b8
MichaelSBaxter <michaelsethbaxter@gmail.com>
2016-09-17 15:09:05 -0400

    Initial commit for ImageViewer project

---
+++ ImageViewer/ImageManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageViewer
+{
+    class ImageManager : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //private BackgroundWorker imageLoader;
+
+        /*private string currentImage;
+        public string CurrentImage
+        {
+            get { return currentImage; }
+            private set
+            {
+                currentImage = value;
+                OnPropertyChanged("CurrentImage");
+            }
+        }*/
+
+        private int currentImageHeight;
+        public int CurrentImageHeight
+        {
+            get { return currentImageHeight; }
+            private set
+            {
+                currentImageHeight = value;
+                OnPropertyChanged("CurrentImageHeight");
+            }
+        }
+
+        private int currentImageWidth;
+        public int CurrentImageWidth
+        {
+            get { return currentImageWidth; }
+            private set
+            {
+                currentImageWidth = value;
+                OnPropertyChanged("CurrentImageWidth");
+            }
+        }
+
+        private ImageSource currentImage;
+        public ImageSource CurrentImage
+        {
+            get { return currentImage; }
+            private set
+            {
+                currentImage = value;
+                OnPropertyChanged("CurrentImage");
+            }   
+        }
+
+        //private bool isLoading = false;
+
+        public ImageManager()
+        {
+            currentImage = null;
+            currentImageWidth = 0;
+            currentImageHeight = 0;
+
+            //imageLoader = new BackgroundWorker();
+            //imageLoader.WorkerSupportsCancellation = false;
+            //imageLoader.WorkerReportsProgress = false;
+            //imageLoader.DoWork += new DoWorkEventHandler(imageLoader_DoWork);
+        }
+
+        public void LoadImage(string filePath)
+        {
+            CurrentImage = new BitmapImage(new Uri(filePath));
+            
+            CurrentImageHeight = (int)CurrentImage.Height;
+            CurrentImageWidth = (int)CurrentImage.Width;
+        }
+
+        /*private void imageLoader_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            if(e.Argument is string)
+            {
+                
+                string filePath = e.Argument as string;
+                ImageSource newImage = new BitmapImage(new Uri(filePath));
+                Application.Current.Dispatcher.Invoke(new Action(() => CurrentImage = newImage));
+            }  
+        }
+
+        private void imageLoader_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            isLoading = false;
+        }*/
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+                propertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ ImageViewer/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ImageManager imageManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {             
+                imageManager = this.FindResource("imageManager") as ImageManager;
+            }
+            catch(ResourceReferenceKeyNotFoundException e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void MenuItem_Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+
+            openDialog.Filter = "Image Files (.jpg)|*.jpg";
+            openDialog.Title = "Select an Image File";
+            openDialog.FilterIndex = 0;
+            openDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            var result = openDialog.ShowDialog();
+            if(result.GetValueOrDefault() == true)
+            {
+                imageManager.LoadImage(openDialog.FileName);
+            }
+        }
+    }
+}
